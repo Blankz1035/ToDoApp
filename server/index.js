1af80784c78ed2a4ddaf38097b0b6d475519d44f
Blankz1035 <andrew.blankley@hotmail.com>
2024-07-19 16:52:50 +0100

    index.js imports and db file for postgres table creation

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 // Entry point to our application.
 
-const express = require("express")
+const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Middleware handler
+app.use(cors());
+app.use(express.json()); // able to get json data from incoming request object
+
 
 app.listen(5000, () => {
     console.log("Server started")
